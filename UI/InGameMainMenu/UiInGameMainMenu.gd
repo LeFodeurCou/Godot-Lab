efc5a9f84bb57f5cc99e2497285825910f3084d4
LeFodeurCou <lefodeurcou@gmail.com>
2026-03-29 19:19:36 +0200

    Some animations improvements / finitions here for the In Game Main Menu

--- UI/InGameMainMenu/UiInGameMainMenu.gd
+++ UI/InGameMainMenu/UiInGameMainMenu.gd
@@ -2,13 +2,15 @@ extends UiBase
 
 class_name UiInGameMainMenu
 
-# Called when the node enters the scene tree for the first time.
+var originalPosY: float
+
 func _ready() -> void:
 	visible = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	$VBoxContainer/ResumeButton.pressed.connect(_onResumePressed)
 	$VBoxContainer/ExitButton.pressed.connect(_onQuitPressed)
 	$ExitConfirmationDialog.confirmed.connect(_onQuitConfirmed)
+	originalPosY = $VBoxContainer.position.y
 
 func _onResumePressed() -> void:
 	requestClose()
@@ -24,30 +26,28 @@ func open() -> void:
 	if pauseAllowed:
 		get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	enterAnimation()
+	await enterAnimation()
 
 	
 func close() -> void:
 	if pauseAllowed:
 		get_tree().paused = false
 	Input.set_mouse_mode(previousMouseMode)
-	outAnimation()
+	await outAnimation()
+	$VBoxContainer.position.y = originalPosY
+	self.visible = false
 	
-func enterAnimation() -> void:
-	var originalPosY = $VBoxContainer.position.y
+func enterAnimation() -> Signal:
 	$VBoxContainer.position.y = -300
 	var tween = create_tween()
 	tween.tween_property($VBoxContainer, "position:y", originalPosY, 0.3)\
 		.set_trans(Tween.TRANS_CUBIC)\
 		.set_ease(Tween.EASE_OUT)
+	return tween.finished
 
-func outAnimation() -> void:
-	var originalPosY = $VBoxContainer.position.y
+func outAnimation() -> Signal:
 	var tween = create_tween()
 	tween.tween_property($VBoxContainer, "position:y", -300, 0.3)\
 		.set_trans(Tween.TRANS_CUBIC)\
 		.set_ease(Tween.EASE_OUT)
-	tween.finished.connect(func():
-		$VBoxContainer.position.y = originalPosY
-		self.visible = false
-	)
+	return tween.finished
